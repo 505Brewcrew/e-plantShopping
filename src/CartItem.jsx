d68a6eb2241c79e15fe8cfbc749f8c326aecc499
kody martin <kody.martin35@gmail.com>
2025-05-08 20:59:00 -0400

    pushing updates

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import  { useSelector, useDispatch }  from  'react-redux';
+import  { removeItem } from  './CartSlice';
+import { updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -8,28 +9,60 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+const calculateTotalAmount = (cart) => {
+    let total = 0;
+  
+    cart.forEach((item) => {
+      // Extract the numeric value from the item's cost string
+      const unitPrice = parseFloat(item.cost.substring(1));
+      
+      // Calculate the total cost for the item
+      const itemTotalCost = item.quantity * unitPrice;
+      
+      // Add the item's total cost to the overall total
+      total += itemTotalCost;
+    });
+  
+    return total;
   };
 
-  const handleContinueShopping = (e) => {
-   
+const handleContinueShopping = (e) => {
+    // Call the onContinueShopping function passed from the parent component
+    onContinueShopping(e);
+  };
+const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
-
-
-  const handleIncrement = (item) => {
+const handleIncrement = (item) => {
+    // Dispatch the updateQuantity action to increase the item quantity by 1
+    dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }));
+  };
+  
+const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      // Dispatch the updateQuantity action to decrease the item quantity by 1
+      dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }));
+    } else {
+      // If the quantity is 1, dispatch the removeItem action to remove the item from the cart
+      dispatch(removeItem(item.id));
+    }
   };
 
-  const handleDecrement = (item) => {
-   
-  };
-
-  const handleRemove = (item) => {
+const handleRemove = (item) => {
+    // Dispatch the removeItem action to delete the item from the cart
+    dispatch(removeItem(item.id));
   };
 
   // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
+const calculateTotalCost = (item) => {
+    // Extract the numeric value from the item's cost string
+const unitPrice = parseFloat(item.cost.substring(1));
+  
+    // Calculate the total cost by multiplying the quantity with the unit price
+const totalCost = item.quantity * unitPrice;
+    
+    return totalCost;
   };
 
   return (
@@ -63,6 +96,4 @@ const CartItem = ({ onContinueShopping }) => {
   );
 };
 
-export default CartItem;
-
-
+export default CartItem
